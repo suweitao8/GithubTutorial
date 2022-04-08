0751f81158cc0b0a2dd880a1555c3e863aecc206
suwietao8 <suweitao8@gmail.com>
2022-04-09 01:18:13 +0800

    Hello World

--- Assets/Scripts/PlayerControoler.cs
+++ Assets/Scripts/PlayerControoler.cs
@@ -7,7 +7,7 @@ public class PlayerControoler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print($"Hello World");
     }
 
     // Update is called once per frame
